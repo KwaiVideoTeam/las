da29703c5eeb4cd4f1d3188ccfc98e28ec125e5b
johnwenjunwu <wuwenjun@pku.edu.cn>
2020-11-07 10:32:58 +0800

    feat(transcode): support input stream's loop

--- transcode/las-transcode.sh
+++ transcode/las-transcode.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ffmpeg -i in.flv \
+ffmpeg -re -stream_loop -1 -i in.flv \
         -vf scale=1280:720 -c:v libx264 -b:v 2000k -x264-params "no-scenecut=1:threads=8:keyint=90" -c:a copy -f flv -y rtmp://127.0.0.1/livestream/out0 \
         -vf scale=960:540  -c:v libx264 -b:v 1000k -x264-params "no-scenecut=1:threads=8:keyint=90" -c:a copy -f flv -y rtmp://127.0.0.1/livestream/out1 \
         -vf scale=640:360  -c:v libx264 -b:v  500k -x264-params "no-scenecut=1:threads=8:keyint=90" -c:a copy -f flv -y rtmp://127.0.0.1/livestream/out2
